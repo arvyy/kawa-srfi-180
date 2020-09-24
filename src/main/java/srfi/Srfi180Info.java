ed7a4af771db5ef7bde7a033613340d905ad003e
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2020-09-24 22:59:15 +0300

    add pom deployment config

---
+++ src/main/java/srfi/Srfi180Info.java
@@ -0,0 +1,7 @@
+package srfi;
+
+/**
+ * For source code, see https://github.com/arvyy/kawa-srfi-180
+ * For SRFI spec and default implementation, see https://srfi.schemers.org/srfi-180/srfi-180.html
+ */
+public class Srfi180Info {}
